f352a0a3e8a193ee88f3144c53016f8e5243e0cd
BABATOUNDE MOCTARD <54064640+BeObm@users.noreply.github.com>
2025-05-12 10:19:33 +0200

    update

--- Baselines/baseline_models.py
+++ Baselines/baseline_models.py
@@ -20,7 +20,7 @@ def baseline_model(model_name, num_classes):
     elif model_name == "GoogleNet":
         return GoogleNet_Model(num_classes)
     elif model_name == "Unet":
-        return Unet_Model(num_classes)
+        return UNetClassifier(num_classes=num_classes)
 
 
 
@@ -146,3 +146,45 @@ def GoogleNet_Model(num_classes):
         nn.Linear(256, num_classes)
     )
     return model
+
+
+class UNetClassifier(nn.Module):
+    def __init__(self, in_channels=3, num_classes=10):
+        super(UNetClassifier, self).__init__()
+
+        def conv_block(in_c, out_c):
+            return nn.Sequential(
+                nn.Conv2d(in_c, out_c, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True)
+            )
+
+        self.encoder1 = conv_block(in_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.encoder2 = conv_block(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.encoder3 = conv_block(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+
+        self.bottleneck = conv_block(256, 512)
+
+        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.encoder1(x)
+        x = self.encoder2(self.pool1(x))
+        x = self.encoder3(self.pool2(x))
+        x = self.bottleneck(self.pool3(x))
+        x = self.global_pool(x)
+        x = self.classifier(x)
+        return x
